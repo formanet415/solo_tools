174799eeb16d87f60d86259f8b55ab4f4826fad5
Tom <58693041+formanet415@users.noreply.github.com>
2022-09-07 13:29:51 +0200

    Update load_pas_vdf_fac.m
    it works now
    minor changes, added output, better comments

--- load_pas_vdf_fac.m
+++ load_pas_vdf_fac.m
@@ -1,7 +1,10 @@
-function  dt = load_pas_vdf_fac(date, deltat)
+function  out = load_pas_vdf_fac(date, deltat)
 %LOAD_FAC_PAS  Transforms PAS into field aligned coordinates
-%   Detailed explanation goes here
+%   Returns array containing the paralell velocity and perpendicular
+%   velocity as coordinates and the number of protons.
+%   out = [(parvel, pervel, np) x (record number) x (number of pixels * number of energy bins)]
 
+%#ok<*AGROW>
 caa_data_paths
 ravg = 3; % minimum averaged mag data points
 % load ing mag data or LL02 mag data
@@ -9,7 +12,7 @@ ravg = 3; % minimum averaged mag data points
 if isempty(b_ep)
     [b_ep, b_vec] = caadb_get_solo_mag_LL02(date, deltat, 'srf');
     if isempty(b_ep)
-        dt = [];
+        out = [];
         return
     end
 end
@@ -22,7 +25,7 @@ b_vec = M*b_vec; % Transform into PAS coordinates
 bn = b_vec./vecnorm(b_vec); % normalize
 
 % loading PAS
-[ep, vdf, elev, energies, azim, extras] = caadb_get_solo_swa_pas_vdf(date, deltat);
+[ep, vdf, elev, energies, azim, ~] = caadb_get_solo_swa_pas_vdf(date, deltat);
 elevs = repmat(elev, 1, 11); % rows change
 azims = repmat(azim',1, 9)'; % columns change
 
@@ -33,20 +36,20 @@ end
 [x,y,z] = sph2cart(deg2rad(azims), deg2rad(elevs), 1*ones(9,11));
 vs(1,:,:) = x; vs(2,:,:) = y; vs(3,:,:) = z;
 cosphi = pagemtimes(bni',vs); % multiplication factor paralell to B
-sinphi = sin(acos(cosphi)); % multiplication factor orthogonal to B
+sinphi = sin(acos(cosphi)); % multiplication factor perpendicular to B
 vels = ev_to_vel(energies);
 
 vdf = permute(vdf,[2 4 1 3]);
-vper = [];
+vpar = [];
 vort = [];
 nums = [];
 for i = 1:length(vels)
     vel = vels(i);
-    vper = [vper reshape(vel*cosphi,length(ep),[])];
-    vort = [vort reshape(vel*sinphi,length(ep),[])];
-    nums = [nums reshape(vdf(i,:,:,:),length(ep),[])];
+    vpar = [vpar reshape(vel*cosphi,length(ep),[])]; % paralell velocity coordiate
+    vort = [vort reshape(vel*sinphi,length(ep),[])]; % perpendicular velocity coordinate
+    nums = [nums reshape(vdf(i,:,:,:),length(ep),[])]; % number of protons
 end
 
-
+out(1,:,:) = vpar; out(2,:,:) = vort; out(3,:,:) = nums;
 end
 
